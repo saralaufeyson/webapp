fe767e5bbadf868cf6808a40a3667ec2fddf2a64
laufey <83583363+saralaufeyson@users.noreply.github.com>
2023-03-12 12:40:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <b>vikasana</b> <button align="center">+ new project</button>
+        <b>vikasana</b> <br/> <button align="center">+ new project</button></br>
         <button>**update**</button>
       </header>
     </div>
